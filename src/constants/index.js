5868b1ea70dcddee4afe819211b6d07c5fb6a7cb
Darshak Kachchhi <87572369+darshak-k@users.noreply.github.com>
2024-01-10 22:31:03 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -130,9 +130,10 @@ const experiences = [
     iconBg: "#E6DEDD",
     date: "November 2023 - Present",
     points: [
-      "Developed new features in the key modules, including contact management, invoicing, and sales, within a CRM/ERP application using Java, Python, Spring boot, Angular, and PostgreSQL technologies",
-      "Enhanced CRM open-source app with dynamic charts, customizable dashboards, and interactive dashlets. Implemented language customization for a tailored user experience, optimizing data visualization and accessibility",
-      "Continuously improving system functionality by addressing new features and resolving bugs, leading to enhanced robustness and reliability in the CRM/ERP application.",
+      "Enhanced and extended functionalities in the 5+ key modules, including base, contact, invoicing, and sales, within an open-source CRM application using Java, Groovy, Spring boot, React, and PostgreSQL technologies",
+      "Enhanced CRM app with dynamic charts, customizable dashboards, interactive dashlets, language customization for a tailored user experience, optimizing data visualization and accessibility."
+      "Improved website performance and speed through optimization techniques by 20%"
+      "Employed the aspect-oriented programming (AOP) design pattern to eliminate cross-cutting concerns, reducing code clutter and duplication by 35%"
     ],
   },
   {
@@ -142,10 +143,11 @@ const experiences = [
     iconBg: "#E6DEDD",
     date: "Aug 2019 - Dec 2021",
     points: [
-      "Developed new back-end micro-services in Java, Python, Spring Boot, and Hibernate/Spring Data JPA.",
-      "Implemented and managed AWS DevOps pipelines to automate software delivery, infrastructure provisioning, and deployment processes.",
-      "Collaborated to code reviews, design discussions, debugging, ensuring codebase quality and adherence to standards.",
-      "Engineered and optimized APIs and event streaming solutions using Kafka, enabling the processing of 100K+ events daily.",
+      "Developed new back-end micro-services in Java, Spring Boot, and Hibernate/Spring Data JPA while creating front-end components in React.",
+      "Successfully integrated and deployed a generic micro-service for file upload with seamless integration capabilities in over 5 production projects, demonstrating its generic nature and adaptability.",
+      "Implemented data streaming solutions for recommendation using Kafka, enabling the processing of 50K+ events daily, and decision-making for stakeholders and product managers.",
+      "Collaborated to code reviews, design discussions, debugging, ensuring codebase quality and adherence to standards, resulting in a significant improvement in continuous delivery.",
+      "Developed comprehensive unit tests to verify the accurate routing of messages to the new topic, achieving a test coverage of 100% and enhancing the reliability of message delivery"
     ],
   }
 ];
